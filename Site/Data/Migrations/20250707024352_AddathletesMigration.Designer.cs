18e1dd6ce34134bdf9d4378191160ac1602e7af8
Niki <Niki@DESKTOP-IFQ7AH3>
2025-07-06 19:57:10 -0700

    2

--- Site/Data/Migrations/20250707024352_AddathletesMigration.Designer.cs
+++ Site/Data/Migrations/20250707024352_AddathletesMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Site.Data;
 
@@ -11,9 +12,11 @@ using Site.Data;
 namespace Site.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250707024352_AddathletesMigration")]
+    partial class AddathletesMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
